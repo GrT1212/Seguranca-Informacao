3c64ff32ed7d8c370aa18576840ec2254601ab19
Victor Souza <victor.souza@treevia.com.br>
2020-07-07 20:43:07 -0300

    Adicionando ON DELETE CASCADE para o caso de uma account ser apagada

--- backend/bin/sql/accountDevice.sql
+++ backend/bin/sql/accountDevice.sql
@@ -1,5 +1,5 @@
 CREATE TABLE accountDevice_tbl(
-  "accountId" INTEGER REFERENCES account_tbl(id),
+  "accountId" INTEGER REFERENCES account_tbl(id) ON DELETE CASCADE,
   "deviceId"  INTEGER REFERENCES device_tbl(id) ON DELETE CASCADE,
   PRIMARY KEY ("accountId", "deviceId")
 );
