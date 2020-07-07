648e82c8d2bfca387766c32837167f5796585122
Victor Souza <victor.souza@treevia.com.br>
2020-07-07 20:57:16 -0300

    Checando se usuario esta autenticado ao ser redirecionado para o frontend

--- frontend/src/components/Root.js
+++ frontend/src/components/Root.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Home from './Home';
 import AuthForm from './AuthForm';
+import { fetchAuthenticated } from '../actions/account';
 
 class Root extends Component {
   render() {
+    if(window.location.pathname == '/auth-redirect')
+    {
+      fetchAuthenticated();
+    }
     return (
       this.props.account.loggedIn ? <Home /> : <AuthForm />
     )
